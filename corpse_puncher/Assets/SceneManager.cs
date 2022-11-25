9f7f705b3f6c071bc0d784b2bb693f6fde0c5cc5
donwung <113479299+donwung@users.noreply.github.com>
2022-11-24 17:27:38 -0800

    added scene manager

---
+++ corpse_puncher/Assets/SceneManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class EditorShortCutKeys : ScriptableObject
+{
+
+    [MenuItem("Edit/Run _F5")] // shortcut key F5 to Play (and exit playmode also)
+    static void PlayGame()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+        }
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("Pause/Play _F6")] // shortcut key F6 to Pause
+    static void PauseGame()
+    {
+        Debug.Break();
+    }
+
+}
