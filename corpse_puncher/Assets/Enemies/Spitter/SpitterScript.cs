15883fea9d4b8ab11c20206d4ee6cd2c0832862c
JeffreyAes <aeschlimanjeffrey@gmail.com>
2022-11-24 22:03:32 -0800

    spitter enemy

--- corpse_puncher/Assets/Enemies/Spitter/SpitterScript.cs
+++ corpse_puncher/Assets/Enemies/Spitter/SpitterScript.cs
@@ -7,6 +7,12 @@ public class SpitterScript : MonoBehaviour
 {
 
     public float raydist = 20f;
+    public GameObject SpitterMaterial;
+    public float minimumDistance;
+    public float Speed;
+    public float timeBetweenLaunch;
+    public float nextLaunchTime;
+    public Transform Player;
     void Start()
     {
         //get player's position here
@@ -14,6 +20,23 @@ public class SpitterScript : MonoBehaviour
 
     void Update()
     {
+            // lauching slime code 
+        if(Time.time > nextLaunchTime){
+            Instantiate(SpitterMaterial, transform.position, Quaternion.identity);
+            nextLaunchTime = Time.time + timeBetweenLaunch;
+        }
+
+        //rotates direction towards the player:
+        if (Player != null){
+
+        transform.LookAt(Player);
+        // if the player is (distance) away from spitter it moves the opposite direction
+        if (Vector3.Distance(transform.position, Player.position) <  minimumDistance){
+            transform.position = Vector3.MoveTowards(transform.position, Player.position, Speed * Time.deltaTime);
+        }
+
+        }
+
         //update player's position here
 
         RaycastHit hit; //hit is of type RaycastHit
